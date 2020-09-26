15ff07a1bf4e409cb757d53958fcecc30d499380
Jabr0ski <51535527+Jabr0ski@users.noreply.github.com>
2020-09-26 13:22:19 +0800

    Beginnings of TTS
    Installed tts library to speak written kiwi sentence.

--- Fun/Kiwi.py
+++ Fun/Kiwi.py
@@ -1,5 +1,8 @@
 import re
+import pyttsx3
+KiwiAcc = pyttsx3.init()
 
+print(KiwiAcc.getProperty('voices'))
 def kiwifi(f):
     sub_dict = {'a':'e','e':'i','i':'u','o':'u','u':'a'}
     regex = '|'.join(sub_dict)
@@ -9,6 +12,8 @@ def main():
     f = input("Type out a word or sentence you want Kiwified. \n")
     newKiwi = kiwifi(f)
     print(newKiwi + "\n")
+    KiwiAcc.say(newKiwi)
+    KiwiAcc.runAndWait()
     main()
 
 main()
