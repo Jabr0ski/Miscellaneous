c3ceb50ffacf2694b3b1102f95b2d83a2aede14d
Jabr0ski <51535527+Jabr0ski@users.noreply.github.com>
2020-10-19 23:12:12 +0800

    Made TTS Optional

--- Fun/Kiwi.py
+++ Fun/Kiwi.py
@@ -2,7 +2,7 @@ import re
 import pyttsx3
 KiwiAcc = pyttsx3.init()
 
-print(KiwiAcc.getProperty('voices'))
+#print(KiwiAcc.getProperty('voices'))
 def kiwifi(f):
     sub_dict = {'a':'e','e':'i','i':'u','o':'u','u':'a'}
     regex = '|'.join(sub_dict)
@@ -11,9 +11,12 @@ def kiwifi(f):
 def main():
     f = input("Type out a word or sentence you want Kiwified. \n")
     newKiwi = kiwifi(f)
+    b = input("TTS? Input yes or no. May not work on all devices. \n")
     print(newKiwi + "\n")
-    KiwiAcc.say(newKiwi)
-    KiwiAcc.runAndWait()
+    b = b.lower()
+    if(b == "y" or b == "yes"):
+        KiwiAcc.say(newKiwi)
+        KiwiAcc.runAndWait()
     main()
 
 main()
